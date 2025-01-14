25afda2df08cd50158d6ca2ec6a883a5a2b3efdd
Dinas Suhadi <dinasputra@gmail.com>
2025-01-14 10:43:18 +0800

    Fix profile to display user profile and avatar

--- profile_widget.php
+++ profile_widget.php
@@ -1,56 +1,48 @@
 <?php
-include_once("includes/db_func.php");
+// session_start();
+// echo "Session list<br/>";
+// foreach ($_SESSION as $key=>$value){
+//     echo "<b>".$key."</b>".": ".$value."<br/>";
+// }
 
+$qry = "SELECT * FROM users WHERE id = $user_id";
+// echo $qry;
+$res = mysqli_query($conn, $qry);
+while ($row = mysqli_fetch_array($res)){
+   $user_fullname = $row['fullname'];
+   $designation = $row['designation'];
+   $user_email = $row['email'];
+   $user_avatar = $row['avatar'];
+}
 ?>
                <!-- Profile Widget -->
-               <div id="_dm-mainnavProfile" class="mainnav__widget my-3 hv-outline-parent">
+                <!-- <style>
+                  .dropdown-toggle{
+                     white-space:normal;
+                  }
+                </style> -->
+                <div id="_dm-mainnavProfile" class="mainnav__widget my-3 hv-outline-parent">
 
-                  <!-- Profile picture  -->
-                  <div class="mininav-toggle text-center py-2">
-                     <img class="mainnav__avatar img-md rounded-circle hv-oc" src="assets/img/profile-photos/1.png" alt="Profile Picture">
-                  </div>
-
-
-                  <div class="mininav-content collapse d-mn-max">
-                     <span data-popper-arrow class="arrow"></span>
-                     <div class="d-grid">
-
-                        <!-- User name and position -->
-                        <button class="mainnav-widget-toggle d-block btn border-0 p-2" data-bs-toggle="collapse" data-bs-target="#usernav" aria-expanded="false" aria-controls="usernav">
-                           <span class="dropdown-toggle d-flex justify-content-center align-items-center">
-                              <h5 class="mb-0 me-3">Aaron Chavez</h5>
-                           </span>
-                           <small class="text-body-secondary">Administrator</small>
-                        </button>
+<!-- Profile picture  -->
+<div class="mininav-toggle text-center py-2">
+   <img id="user-avatar-img" class="mainnav__avatar img-md rounded-circle hv-oc" src="assets/img/<?php echo $user_avatar?'uploads/'.$user_avatar:'profile-photos/3.png' ?>" alt="Profile Picture">
+</div>
 
 
-                        <!-- Collapsed user menu -->
-                        <div id="usernav" class="nav flex-column collapse">
-                           <a href="#" class="nav-link d-flex justify-content-between align-items-center">
-                              <span><i class="demo-pli-mail fs-5 me-2"></i><span class="ms-1">Messages</span></span>
-                              <span class="badge bg-danger rounded-pill">14</span>
-                           </a>
-                           <a href="#" class="nav-link">
-                              <i class="demo-pli-male fs-5 me-2"></i>
-                              <span class="ms-1">Profile</span>
-                           </a>
-                           <a href="#" class="nav-link">
-                              <i class="demo-pli-gear fs-5 me-2"></i>
-                              <span class="ms-1">Settings</span>
-                           </a>
-                           <a href="#" class="nav-link">
-                              <i class="demo-pli-computer-secure fs-5 me-2"></i>
-                              <span class="ms-1">Lock screen</span>
-                           </a>
-                           <a href="#" class="nav-link">
-                              <i class="demo-pli-unlock fs-5 me-2"></i>
-                              <span class="ms-1">Logout</span>
-                           </a>
-                        </div>
+<div class="mininav-content collapse d-mn-max">
+   <span data-popper-arrow class="arrow"></span>
+   <div class="d-grid">
 
+      <!-- User name and position -->
+      <button class="mainnav-widget-toggle d-block btn border-0 p-2" data-bs-toggle="collapse" data-bs-target="#usernav" aria-expanded="false" aria-controls="usernav">
+         <span class="d-flex justify-content-center align-items-center">
+            <h5 class="mb-0" id="user-profile-fullname"><?php echo $user_fullname; ?></h5>
+         </span>
+         <small class="text-body-secondary" id="user-profile-nickname"><?php echo $designation; ?></small>
+      </button>
+      
+   </div>
+</div>
 
-                     </div>
-                  </div>
-
-               </div>
-               <!-- End - Profile widget -->
+</div>
+<!-- End - Profile widget -->
