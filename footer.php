fec3483a345d8ef3f75066eed4fef017c0bf2986
Dinas Suhadi <dinasputra@gmail.com>
2025-01-14 10:40:49 +0800

    Edit footer company name

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
          <footer class="mt-auto">
             <div class="content__boxed">
                <div class="content__wrap py-3 py-md-1 d-flex flex-column flex-md-row align-items-md-center">
-                  <div class="text-nowrap mb-4 mb-md-0">Copyright &copy; <?php echo date('Y')=='2024'?'2024':'2024 - '.date('Y'); ?> <a href="#" class="ms-1 btn-link fw-bold">Short Circuit Works</a></div>
+                  <div class="text-nowrap mb-4 mb-md-0">Copyright &copy; <?php echo date('Y')=='2024'?'2024':'2024 - '.date('Y'); ?> <a href="#" class="ms-1 btn-link fw-bold">Setegap Venture Petroleum Sdn. Bhd.</a></div>
                   <nav class="nav flex-column gap-1 flex-md-row gap-md-3 ms-md-auto">
                      <a class="nav-link link-offset-3 link-underline-hover px-0" href="#">Policy Privacy</a>
                      <a class="nav-link link-offset-3 link-underline-hover px-0" href="#">Terms and conditions</a>
