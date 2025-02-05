9443a97bf65bdf1d8282be66fe0736f048cd0ea0
Dinas Suhadi <dinasputra@gmail.com>
2025-02-05 21:08:48 +0800

    Add confirm email page

---
+++ confirmmail-content.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="light" data-scheme="orange">
+
+<head>
+   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+   <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
+   <meta name="description" content="The login page allows a user to gain access to an application by entering their username and password or by authenticating using a social media login.">
+   <title>Registration | SIRIUS-I</title>
+
+
+   <!-- STYLESHEETS -->
+   <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+   <!-- Fonts [ OPTIONAL ] -->
+   
+   
+   <style type="text/css">@font-face {font-family:Poppins;font-style:normal;font-weight:300;src:url(/assets/fonts/poppins/5.0.11/latin/300/normal.woff2);unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD;font-display:swap;}@font-face {font-family:Poppins;font-style:normal;font-weight:300;src:url(/assets/fonts/poppins/5.0.11/latin-ext/300/normal.woff2);unicode-range:U+0100-02AF,U+0304,U+0308,U+0329,U+1E00-1E9F,U+1EF2-1EFF,U+2020,U+20A0-20AB,U+20AD-20CF,U+2113,U+2C60-2C7F,U+A720-A7FF;font-display:swap;}@font-face {font-family:Poppins;font-style:normal;font-weight:300;src:url(/assets/fonts/poppins/5.0.11/devanagari/300/normal.woff2);unicode-range:U+0900-097F,U+1CD0-1CF9,U+200C-200D,U+20A8,U+20B9,U+25CC,U+A830-A839,U+A8E0-A8FF;font-display:swap;}@font-face {font-family:Poppins;font-style:normal;font-weight:400;src:url(/assets/fonts/poppins/5.0.11/latin-ext/400/normal.woff2);unicode-range:U+0100-02AF,U+0304,U+0308,U+0329,U+1E00-1E9F,U+1EF2-1EFF,U+2020,U+20A0-20AB,U+20AD-20CF,U+2113,U+2C60-2C7F,U+A720-A7FF;font-display:swap;}@font-face {font-family:Poppins;font-style:normal;font-weight:400;src:url(/assets/fonts/poppins/5.0.11/devanagari/400/normal.woff2);unicode-range:U+0900-097F,U+1CD0-1CF9,U+200C-200D,U+20A8,U+20B9,U+25CC,U+A830-A839,U+A8E0-A8FF;font-display:swap;}@font-face {font-family:Poppins;font-style:normal;font-weight:400;src:url(/assets/fonts/poppins/5.0.11/latin/400/normal.woff2);unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD;font-display:swap;}@font-face {font-family:Poppins;font-style:normal;font-weight:500;src:url(/assets/fonts/poppins/5.0.11/devanagari/500/normal.woff2);unicode-range:U+0900-097F,U+1CD0-1CF9,U+200C-200D,U+20A8,U+20B9,U+25CC,U+A830-A839,U+A8E0-A8FF;font-display:swap;}@font-face {font-family:Poppins;font-style:normal;font-weight:500;src:url(/assets/fonts/poppins/5.0.11/latin-ext/500/normal.woff2);unicode-range:U+0100-02AF,U+0304,U+0308,U+0329,U+1E00-1E9F,U+1EF2-1EFF,U+2020,U+20A0-20AB,U+20AD-20CF,U+2113,U+2C60-2C7F,U+A720-A7FF;font-display:swap;}@font-face {font-family:Poppins;font-style:normal;font-weight:500;src:url(/assets/fonts/poppins/5.0.11/latin/500/normal.woff2);unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD;font-display:swap;}@font-face {font-family:Poppins;font-style:normal;font-weight:700;src:url(/assets/fonts/poppins/5.0.11/devanagari/700/normal.woff2);unicode-range:U+0900-097F,U+1CD0-1CF9,U+200C-200D,U+20A8,U+20B9,U+25CC,U+A830-A839,U+A8E0-A8FF;font-display:swap;}@font-face {font-family:Poppins;font-style:normal;font-weight:700;src:url(/assets/fonts/poppins/5.0.11/latin/700/normal.woff2);unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD;font-display:swap;}@font-face {font-family:Poppins;font-style:normal;font-weight:700;src:url(/assets/fonts/poppins/5.0.11/latin-ext/700/normal.woff2);unicode-range:U+0100-02AF,U+0304,U+0308,U+0329,U+1E00-1E9F,U+1EF2-1EFF,U+2020,U+20A0-20AB,U+20AD-20CF,U+2113,U+2C60-2C7F,U+A720-A7FF;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:400;src:url(/assets/fonts/ubuntu/5.0.11/greek-ext/400/normal.woff2);unicode-range:U+1F00-1FFF;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:400;src:url(/assets/fonts/ubuntu/5.0.11/cyrillic/400/normal.woff2);unicode-range:U+0301,U+0400-045F,U+0490-0491,U+04B0-04B1,U+2116;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:400;src:url(/assets/fonts/ubuntu/5.0.11/latin/400/normal.woff2);unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:400;src:url(/assets/fonts/ubuntu/5.0.11/greek/400/normal.woff2);unicode-range:U+0370-03FF;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:400;src:url(/assets/fonts/ubuntu/5.0.11/latin-ext/400/normal.woff2);unicode-range:U+0100-02AF,U+0304,U+0308,U+0329,U+1E00-1E9F,U+1EF2-1EFF,U+2020,U+20A0-20AB,U+20AD-20CF,U+2113,U+2C60-2C7F,U+A720-A7FF;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:400;src:url(/assets/fonts/ubuntu/5.0.11/cyrillic-ext/400/normal.woff2);unicode-range:U+0460-052F,U+1C80-1C88,U+20B4,U+2DE0-2DFF,U+A640-A69F,U+FE2E-FE2F;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:500;src:url(/assets/fonts/ubuntu/5.0.11/cyrillic/500/normal.woff2);unicode-range:U+0301,U+0400-045F,U+0490-0491,U+04B0-04B1,U+2116;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:500;src:url(/assets/fonts/ubuntu/5.0.11/latin-ext/500/normal.woff2);unicode-range:U+0100-02AF,U+0304,U+0308,U+0329,U+1E00-1E9F,U+1EF2-1EFF,U+2020,U+20A0-20AB,U+20AD-20CF,U+2113,U+2C60-2C7F,U+A720-A7FF;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:500;src:url(/assets/fonts/ubuntu/5.0.11/greek/500/normal.woff2);unicode-range:U+0370-03FF;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:500;src:url(/assets/fonts/ubuntu/5.0.11/latin/500/normal.woff2);unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:500;src:url(/assets/fonts/ubuntu/5.0.11/greek-ext/500/normal.woff2);unicode-range:U+1F00-1FFF;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:500;src:url(/assets/fonts/ubuntu/5.0.11/cyrillic-ext/500/normal.woff2);unicode-range:U+0460-052F,U+1C80-1C88,U+20B4,U+2DE0-2DFF,U+A640-A69F,U+FE2E-FE2F;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:700;src:url(/assets/fonts/ubuntu/5.0.11/latin-ext/700/normal.woff2);unicode-range:U+0100-02AF,U+0304,U+0308,U+0329,U+1E00-1E9F,U+1EF2-1EFF,U+2020,U+20A0-20AB,U+20AD-20CF,U+2113,U+2C60-2C7F,U+A720-A7FF;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:700;src:url(/assets/fonts/ubuntu/5.0.11/cyrillic/700/normal.woff2);unicode-range:U+0301,U+0400-045F,U+0490-0491,U+04B0-04B1,U+2116;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:700;src:url(/assets/fonts/ubuntu/5.0.11/greek/700/normal.woff2);unicode-range:U+0370-03FF;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:700;src:url(/assets/fonts/ubuntu/5.0.11/greek-ext/700/normal.woff2);unicode-range:U+1F00-1FFF;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:700;src:url(/assets/fonts/ubuntu/5.0.11/latin/700/normal.woff2);unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD;font-display:swap;}@font-face {font-family:Ubuntu;font-style:normal;font-weight:700;src:url(/assets/fonts/ubuntu/5.0.11/cyrillic-ext/700/normal.woff2);unicode-range:U+0460-052F,U+1C80-1C88,U+20B4,U+2DE0-2DFF,U+A640-A69F,U+FE2E-FE2F;font-display:swap;}</style>
+
+
+   <!-- Bootstrap CSS [ REQUIRED ] -->
+   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+   <!-- Nifty CSS [ REQUIRED ] -->
+   <link rel="stylesheet" href="assets/css/nifty.min.css">
+
+
+   <!-- Favicons [ OPTIONAL ] -->
+   <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+   <link rel="manifest" href="site.webmanifest">
+
+
+   <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+   [ REQUIRED ]
+   You must include this category in your project.
+   -->
+
+   <!-- toastr.js -->
+   <link rel="stylesheet" href="assets/vendors/toastr/toastr.min.css">
+
+   <!--
+   [ OPTIONAL ]
+   This is an optional plugin. You may choose to include it in your project.
+
+
+   [ DEMO ]
+   Used for demonstration purposes only. This category should NOT be included in your project.
+
+
+   [ SAMPLE ]
+   Here's a sample script that explains how to initialize plugins and/or components: This category should NOT be included in your project.
+
+
+   Detailed information and more samples can be found in the documentation.
+
+   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+</head>
+
+<body class="" style="background-image: url('../../assets/premium/boxed-bg/gas-pipeline.jpg');">
+
+
+  <!-- PAGE CONTAINER -->
+  <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+  <div id="root" class="root front-container">
+
+      <!-- CONTENTS -->
+      <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+      <section id="content" class="content">
+        <div class="content__boxed w-100 min-vh-100 d-flex flex-column align-items-center justify-content-center">
+            <div class="content__wrap">
+
+              <!-- Login card -->
+              <div class="card shadow-lg">
+                  <div class="card-body p-4">
+
+                    <!-- <div class="bg-primary rounded mb-3">
+                      <img src="assets/img/SVP-Logo_outline-250x200.png" alt="Setegap Ventures Petroleum Logo">
+                    </div> -->
+                    <div class="text-center">
+                      <img src="assets/img/SVP-Logo_outline-250x200.png" alt="Setegap Ventures Petroleum Logo">
+                        <h1 class="h3">Email verified!</h1>
+                        <p>Your <b>email</b> will be used as your username for SIRIUS-I login. </br>Create your <b>password</b> to complete the registration.</p>
+                    </div>
+
+                    <form class="mt-4 needs-validation" novalidate action="confirmmail.php" method="post">
+                        <input type="hidden" name="action" value="<?php echo $_GET['action']; ?>">
+                        <input type="hidden" name="username" value="<?php echo $email; ?>">
+                        <div class="mb-3">
+                          <input type="text" class="form-control" placeholder="Username" value="Username: <?php echo $email; ?>" disabled>
+                          <div class="invalid-feedback">Please input your username</div>
+                        </div>
+
+                        <div class="row">
+                          <div class="mb-3 col-md-6">
+                            <input type="password" class="form-control" oninput="validatePassword(this.value)" placeholder="Password" name="password1" required autofocus>
+                            <div class="invalid-feedback">Please input your password</div>
+                            <span id="errorMessage" class="font-weight-bold text-danger"></span>
+                            <div class="form-group">
+                                <ul>
+                                    <li id="minLength"><i class="fas fa-times
+                                        text-danger"></i> Minimum 8 characters</li>
+                                    <li id="uppercase"><i class="fas fa-times
+                                        text-danger"></i> At least one uppercase
+                                        letter</li>
+                                    <li id="lowercase"><i class="fas fa-times
+                                        text-danger"></i> At least one lowercase
+                                        letter</li>
+                                    <li id="symbol"><i class="fas fa-times
+                                        text-danger"></i> 
+                                          At least one symbol (@$!%*?&-)
+                                    </li>
+                                </ul>
+                            </div>
+                          </div>
+
+                          <div class="mb-3 col-md-6">
+                            <input type="password" class="form-control" placeholder="Password" id="password2" name="password2" required>
+                            <div id="passwordError" class="invalid-feedback">Password does not match</div>
+                          </div>
+                        </div>
+
+                        <div class="d-grid mt-5">
+                          <button class="btn btn-primary btn-lg" type="submit">Proceed to update details</button>
+                        </div>
+                    </form>
+
+                    <!-- <div class="d-flex justify-content-between gap-md-5 mt-4">
+                        <a href="forgotpass.php" class="btn-link text-decoration-none">Forgot password ?</a>
+                        <a href="register.php" class="btn-link text-decoration-none">Create a new account</a>
+                    </div> -->
+
+                  </div>
+              </div>
+
+              <!-- END : Login card -->
+            </div>
+        </div>
+
+
+      </section>
+
+      <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+      <!-- END - CONTENTS -->
+  </div>
+  <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+  <!-- END - PAGE CONTAINER -->
+
+
+  <!-- JAVASCRIPTS -->
+  <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+  <script src="assets/js/jquery.js"></script>
+
+  <!-- Bootstrap JS [ OPTIONAL ] -->
+  <script src="assets/js/bootstrap.min.js"></script>
+
+  <!-- Nifty JS [ OPTIONAL ] -->
+  <script src="assets/js/nifty.min.js"></script>
+
+  <!-- toastr.js -->
+  <script src="assets/vendors/toastr/toastr.min.js"></script>
+
+  <script>
+      $(document).ready(function(){
+        var toastmsg = '<?php echo $toastmsg; ?>'
+        var toasttype = '<?php echo $toasttype; ?>'
+        toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "progressBar": true,
+        "preventDuplicates": false,
+        "positionClass": "toast-top-center",
+        "onclick": null,
+        "showDuration": "400",
+        "hideDuration": "1000",
+        "timeOut": "7000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+        }
+
+        if(toastmsg!=''){
+              if(toasttype == 'error'){
+                  toastr.error(toastmsg,'SIRIUS-I');
+              }else if(toasttype=='success'){
+                  toastr.success(toastmsg,'SIRIUS-I');
+              }else if(toasttype=='info'){
+                  toastr.info(toastmsg,'SIRIUS-I');
+              }
+        }
+
+        // $('.i-checks').iCheck({
+        //     checkboxClass: 'icheckbox_square-green',
+        //     radioClass: 'iradio_square-green',
+        // });
+        // $('#svpDiv').hide();
+        var theForm;
+        const forms = document.querySelectorAll(".needs-validation");
+        forms.forEach(form => {
+          theForm = form;
+          form.addEventListener("submit", e => {
+              if (!form.checkValidity()) {
+                    e.preventDefault()
+                    e.stopPropagation()
+              }
+              form.classList.add("was-validated")
+          }, false)
+        })
+
+        // Check if passwords match
+        const errorMessage = document.getElementById('passwordError');
+        $("#password2").on("keyup", function () {
+          console.log($("#password2").val());
+          // errorMessage.removeClass('invalid-feedback');
+          if ($("#password1").val() == $("#password2").val()) {
+            console.log('matched');
+            errorMessage.textContent = 'Password matched';
+            errorMessage.classList.remove('invalid-feedback');
+            errorMessage.classList.add('is-valid');
+          }else{
+            // theForm.classList.add("was-validated")
+            console.log('not matched');
+            errorMessage.textContent = 'Password does not match';
+            errorMessage.classList.remove('is-valid');
+            errorMessage.classList.add('is-invalid');
+          }
+        });
+
+      });
+
+
+      function validatePassword(password) {
+            const strongPasswordRegex = 
+                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&-])[A-Za-z\d@$!%*?&-]{8,}$/;
+            const errorMessage = document.getElementById('errorMessage');
+
+            // Check each condition and update the corresponding label
+            document.getElementById('minLength').innerHTML = 
+                  password.length >= 8 ?
+                '<i class="fas fa-check text-success"></i> Minimum 8 characters' :
+                '<i class="fas fa-times text-danger"></i> Minimum 8 characters';
+            document.getElementById('uppercase').innerHTML = 
+                  /[A-Z]/.test(password) ?
+                '<i class="fas fa-check text-success"></i> At least one uppercase letter' :
+                '<i class="fas fa-times text-danger"></i> At least one uppercase letter';
+            document.getElementById('lowercase').innerHTML = 
+                  /[a-z]/.test(password) ?
+                '<i class="fas fa-check text-success"></i> At least one lowercase letter' :
+                '<i class="fas fa-times text-danger"></i> At least one lowercase letter';
+            document.getElementById('symbol').innerHTML = 
+                  /[@$!%*?&-]/.test(password) ?
+                '<i class="fas fa-check text-success"></i> At least one symbol (@$!%*?&-)' :
+                '<i class="fas fa-times text-danger"></i> At least one symbol (@$!%*?&-)';
+
+            // Check overall validity and update the error message
+            if (strongPasswordRegex.test(password)) {
+                errorMessage.textContent = 'Strong Password';
+                errorMessage.classList.remove('text-danger');
+                errorMessage.classList.add('text-success');
+            } else {
+                errorMessage.textContent = 'Weak Password';
+                errorMessage.classList.remove('text-success');
+                errorMessage.classList.add('text-danger');
+            }
+        }
+    </script>
+
+</body>
+</html>
---
+++ confirmmail.php
@@ -0,0 +1,87 @@
+<?php
+// session_start();
+require_once 'includes/db_func.php';
+$toastmsg = '';
+$toasttype = '';
+if (isset($_SESSION['message']) && $_SESSION['message']!= ''){
+    $toastmsg = $_SESSION['message'];
+}
+if(isset($_SESSION['msgtype'])&&$_SESSION['msgtype']!=''){
+    $toasttype = $_SESSION['msgtype'];
+}
+
+unset($_SESSION['message']);
+unset($_SESSION['msgtype']);
+
+$error="";
+if (isset($_GET["key"]) && isset($_GET["email"]) && isset($_GET["action"]) && ($_GET["action"]=="confirm") && !isset($_POST["action"])){
+  $key = $_GET["key"];
+  $email = $_GET["email"];
+  $curDate = date("Y-m-d H:i:s");
+  $qry = "SELECT * FROM `users` WHERE `hash_id`='".$key."' and `username`='".$email."' AND `confirm` = 0";
+  // die( $qry);
+  $query = mysqli_query($conn, $qry );
+  $row = mysqli_num_rows($query);
+  // echo $row;
+  if ($row==0){
+    $error .= '<h2>Invalid Link</h2>
+    <p>The link is invalid/expired. Either you did not copy the correct link
+    from the email, or you have already used the key in which case it is 
+    deactivated.</p>
+    <p><a href="forgotpass.php">
+    Click here</a> to reset password.</p>';
+    $_SESSION['message'] = "The link is invalid/expired. <br/>Please contact SIRIUS-I administrator.";
+    $_SESSION['msgtype'] = "error";
+	}else{
+    $row = mysqli_fetch_assoc($query);
+    $user_id = $row['id'];
+    $expDate = $row['exp_date'];
+    if ($expDate >= $curDate){
+      include_once 'confirmmail-content.php';
+      $_SESSION['user_id'] = $user_id;
+    }else{
+      // echo ("Error link expired below");
+      $error .= "<h2>Link Expired</h2>
+      <p>The link is expired. You are trying to use the expired link which 
+      is valid only 24 hours (1 days after request).<br /><br /></p>";
+      $_SESSION['message'] = "The link is expired after 24 hours. Please register again.";
+      $_SESSION['msgtype'] = "error";
+
+      $qry = "DELETE FROM `login` WHERE `hash_id`='".$key."' and `username`='".$email."' AND `confirm` = 0";
+      // die($qry);
+      mysqli_query($conn, $qry);
+    }
+  }
+  if($error!=""){
+    // echo "<div class='error'>".$error."</div><br />";
+    header('Location: index.php');
+    // header('Location: sesstest.php');
+  }		
+  // isset email key validate end	
+}elseif(isset($_POST["username"]) && isset($_POST["action"]) && ($_POST["action"]=="confirm")){
+  // echo "form post";
+  $pass1 = mysqli_real_escape_string($conn,$_POST["password1"]);
+  $pass2 = mysqli_real_escape_string($conn,$_POST["password2"]);
+  $email = $_POST["username"];
+  $curDate = date("Y-m-d H:i:s");
+  if ($pass1!=$pass2){
+    $error.= "<p>Password do not match, both password should be same.<br /><br /></p>";
+  }
+  if($error!=""){
+    echo "<div class='error'>".$error."</div><br />";
+  }else{
+    $pwd = password_hash($pass1, PASSWORD_DEFAULT);
+    mysqli_query($conn, "UPDATE `login` SET `password`='".$pwd."', `confirm` = 1 WHERE `username`='".$email."';");
+
+    // mysqli_query($conn,"DELETE FROM `forgot_password` WHERE `email`='".$email."';");
+    
+    $_SESSION['message'] = "Congratulations! Your password has been updated successfully.";
+    $_SESSION['msgtype'] = "success";
+    header('Location: wizard.php?first=1');
+
+    // echo '<div class="error"><p>Congratulations! Your password has been updated successfully.</p>
+    // <p><a href="index.php">
+    // Click here</a> to Login.</p></div><br />';
+  }
+}
+?>
