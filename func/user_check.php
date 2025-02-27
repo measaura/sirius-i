889a7fd209c46ef1f2e7680a03f63ceba9c0d842
Dinas Suhadi <dinasputra@gmail.com>
2025-02-27 16:20:09 +0800

    Update user_check to check for logged in user

--- func/user_check.php
+++ func/user_check.php
@@ -6,19 +6,24 @@
 // }
 if(isset($_SESSION['uid']) && $_SESSION['uid'] != ''){
     $user_id=$_SESSION['uid'];
+    $qry = "SELECT * FROM users WHERE id = $user_id";
+    // echo $qry;
+    $res = mysqli_query($conn, $qry);
+    while ($row = mysqli_fetch_array($res)){
+       $user_fullname = $row['fullname'];
+       $designation = $row['designation'];
+       $user_email = $row['username'];
+       $user_avatar = $row['avatar'];
+       $user_mobile = $row['mobile'];
+       $user_empid = $row['emp_id'];
+       $user_company = $row['company'];
+    }
+    // if($_SERVER['SCRIPT_NAME'] != '/main.php'){
+    //     header('Location: /main.php');
+    // }
 }else{
-    header('Location: index.php');
-}
-$qry = "SELECT * FROM users WHERE id = $user_id";
-// echo $qry;
-$res = mysqli_query($conn, $qry);
-while ($row = mysqli_fetch_array($res)){
-   $user_fullname = $row['fullname'];
-   $designation = $row['designation'];
-   $user_email = $row['username'];
-   $user_avatar = $row['avatar'];
-   $user_mobile = $row['mobile'];
-   $user_empid = $row['emp_id'];
-   $user_company = $row['company'];
+    if($_SERVER['SCRIPT_NAME'] != '/index.php'){
+        header('Location: /index.php');
+    }
 }
 ?>
